b060b8013ae90de5547d2d838924fe34f935d981
gabriel <gabriel.f.lazari@gmail.com>
2023-03-16 17:59:49 -0300

    bank petal cpf

--- app/validaCpf.js
+++ app/validaCpf.js
@@ -1,4 +1,21 @@
 export default function validaCpf(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    console.log(cpf)
+    validaNumeroRepetidos(cpf)
+}
+
+function validaNumeroRepetidos(cpf) {
+    const numeroRepetidos = [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999'
+    ]
+
+    return numeroRepetidos.includes(cpf);
 }
