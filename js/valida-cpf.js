a918186e447b245b2aea76dc99dff122b65504dd
gabriel <gabriel.f.lazari@gmail.com>
2023-03-18 11:41:20 -0300

    local storage

--- js/valida-cpf.js
+++ js/valida-cpf.js
@@ -1,14 +1,12 @@
-export default function validaCpf(campo) {
+export default function ehUmCPF(campo) {
     const cpf = campo.value.replace(/\.|-/g, "");
-    if (validaNumeroRepetidos(cpf) || validaPenultimoNumeroCpf(cpf) || validaUltimoNumeroCpf(cpf)) {
-        console.log('CPF inválido');
-    } else {
-        console.log('CPF válido');
+    if (validaNumerosRepetidos(cpf) || validaPrimeiroDigito(cpf) || validaSegundoDigito(cpf)) {
+        campo.setCustomValidity('Esse cpf não é válido')
     }
 }
 
-function validaNumeroRepetidos(cpf) {
-    const numeroRepetidos = [
+function validaNumerosRepetidos(cpf) {
+    const numerosRepetidos = [
         '00000000000',
         '11111111111',
         '22222222222',
@@ -21,16 +19,16 @@ function validaNumeroRepetidos(cpf) {
         '99999999999'
     ]
 
-    return numeroRepetidos.includes(cpf);
+    return numerosRepetidos.includes(cpf)
 }
 
-function validaPenultimoNumeroCpf(cpf) {
+function validaPrimeiroDigito(cpf) {
     let soma = 0;
     let multiplicador = 10;
 
-    for (let i = 0; i < 9; i++) {
-        soma += cpf[i] * multiplicador;
-        multiplicador--;
+    for (let tamanho = 0; tamanho < 9; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
     }
 
     soma = (soma * 10) % 11;
@@ -42,13 +40,13 @@ function validaPenultimoNumeroCpf(cpf) {
     return soma != cpf[9];
 }
 
-function validaUltimoNumeroCpf(cpf) {
+function validaSegundoDigito(cpf) {
     let soma = 0;
     let multiplicador = 11;
 
-    for (let i = 0; i < 10; i++) {
-        soma += cpf[i] * multiplicador;
-        multiplicador--;
+    for (let tamanho = 0; tamanho < 10; tamanho++) {
+        soma += cpf[tamanho] * multiplicador;
+        multiplicador--
     }
 
     soma = (soma * 10) % 11;
